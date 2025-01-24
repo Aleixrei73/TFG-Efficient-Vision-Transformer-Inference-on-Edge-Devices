a4ba513d61338a67a9ca0d5483eb691d1974f2bf
Aleixrei73 <aleix.armero73@gmail.com>
2025-01-25 00:21:39 +0100

    Default model trained

---
+++ utils.py
@@ -0,0 +1,122 @@
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
+import torchvision
+from torchvision.datasets import ImageFolder
+from pathlib import Path
+from tqdm.auto import tqdm
+from typing import Dict, List, Tuple
+import matplotlib.pyplot as plt
+
+NUM_WORKERS = os.cpu_count()
+
+def create_loaders(path, transform, batch_size, num_workers=NUM_WORKERS):
+    dataset = torchvision.datasets.CIFAR10(Path(path), train=True, transform=transform, download=True)
+    train_dataset, test_dataset = random_split(dataset, [0.8, 0.2])
+    val_dataset = torchvision.datasets.CIFAR10(Path(path), train=False, transform=transform, download=False)
+    class_names = dataset.classes
+    train_dl = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    test_dl = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    val_dl = DataLoader(
+      val_dataset,
+      batch_size=batch_size,
+      shuffle=False,
+      num_workers=num_workers,
+      pin_memory=True,
+    )
+    return train_dl, test_dl, val_dl, class_names
+
+# Plot loss curves of a model
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+    """
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
+
+    accuracy = results["train_acc"]
+    test_accuracy = results["test_acc"]
+
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+
+"""
+Contains various utility functions for PyTorch model training and saving.
+"""
+import torch
+from pathlib import Path
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+    """Saves a PyTorch model to a target directory.
+
+    Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+
+    Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+    """
+    # Create target directory
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True,
+                        exist_ok=True)
+
+    # Create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+    model_save_path = target_dir_path / model_name
+
+    # Save the model state_dict()
+    print(f"[INFO] Saving model to: {model_save_path}")
+    torch.save(obj=model.state_dict(),
+             f=model_save_path)
+
+
+def load_default_model():
+    model_path = Path("model/ViT-Default.pht")
+    vit = torchvision.models.vit_b_16()
+    vit.heads = nn.Linear(in_features=768, out_features=10)
+    vit.load_state_dict(torch.load(model_path))
+    return vit
