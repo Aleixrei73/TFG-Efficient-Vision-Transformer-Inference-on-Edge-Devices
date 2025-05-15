0c0a39235ed37aeb9313478541cbc6f9682ed771
Aleixrei73 <aleix.armero73@gmail.com>
2025-05-16 00:03:15 +0200

    All combinations done

--- combo_metrics.py
+++ combo_metrics.py
@@ -16,41 +16,40 @@ import simplify
 import torch.nn.utils.prune as prune
 import tome.patch
 
-r = [25, 30]
-p = [0.4998]
+p = [5, 10, 15, 20, 25, 30]
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
 for pruning in p:
-    print(f'prunning = {pruning:.2f}')
-    for merging in r:
-        print(f'r = {merging}')
+    
+    print(f'r = {pruning:.2f}')
 
-        vit_default = utils.load_default_model()
-        loss_fn = nn.CrossEntropyLoss()
-        pretrained_vit_weights = torchvision.models.ViT_B_16_Weights.DEFAULT
-        pretrained_vit_transforms = pretrained_vit_weights.transforms()
+    vit_default = utils.load_default_model()
+    loss_fn = nn.CrossEntropyLoss()
+    pretrained_vit_weights = torchvision.models.ViT_B_16_Weights.DEFAULT
+    pretrained_vit_transforms = pretrained_vit_weights.transforms()
 
-        pruned_model = utils.prune_vit(vit_default, pruning)
-        tome.patch.swag(pruned_model)
-        pruned_model.r = merging
-        pruned_model.load_state_dict(torch.load(f"model/Combination/Pruning{pruning:.1f}/ViT-Combine-Pruning{pruning:.2f}-Merging{merging}-Best.pht", weights_only=True))
+    tome.patch.swag(vit_default)
+    vit_default.r = pruning
+    vit_default.load_state_dict(torch.load(f"model/Merging/ViT-Merging{pruning}-Best.pht", weights_only=True))
 
-        for parameter in pruned_model.parameters():
-            parameter.requires_grad = False
+    for parameter in vit_default.parameters():
+        parameter.requires_grad = False
+           
+    _, _, val_dl, _ = utils.create_loaders("data", transform=pretrained_vit_transforms, batch_size=1, dtype=torch.bfloat16)
+    loss_fn = nn.CrossEntropyLoss()
             
-        _, _, val_dl, _ = utils.create_loaders("data", transform=pretrained_vit_transforms, batch_size=1)
-        loss_fn = nn.CrossEntropyLoss()
+    vit_default = vit_default.to(torch.bfloat16)
             
-        torch.manual_seed(0)
-        torch.cuda.manual_seed(0)
-        metrics = trainer.getMetrics(pruned_model, val_dl, device, num_times=2000, save_plots=True, model_title=f'ViT-Combine-Pruning{pruning:.1f}-Merging{merging}')
+    torch.manual_seed(0)
+    torch.cuda.manual_seed(0)
+    metrics = trainer.getMetrics(vit_default, val_dl, device, num_times=2000, save_plots=True, model_title=f'ViT-Combine-Merging{pruning}-Quantized')
 
-        _, _, val_dl, _ = utils.create_loaders("data", transform=pretrained_vit_transforms, batch_size=129)
-        torch.manual_seed(0)
-        torch.cuda.manual_seed(0)
-        val_acc = trainer.evaluate(pruned_model, val_dl, loss_fn, device)
+    _, _, val_dl, _ = utils.create_loaders("data", transform=pretrained_vit_transforms, batch_size=129, dtype=torch.bfloat16)
+    torch.manual_seed(0)
+    torch.cuda.manual_seed(0)
+    val_acc = trainer.evaluate(vit_default, val_dl, loss_fn, device)
 
-        with open(f'summary/ViT-Combine-Pruning{pruning:.1f}-Merging{merging}/metrics.txt', "a") as f:
-            f.write(f'\nAccuracy: {(val_acc*100):.2f}%')
+    with open(f'summary/ViT-Combine-Merging{pruning}-Quantized/metrics.txt', "a") as f:
+        f.write(f'\nAccuracy: {(val_acc*100):.2f}%')
--- table_writer.py
+++ table_writer.py
@@ -1,1 +1,30 @@
 
+r = [5, 10, 15, 20, 25, 30]
+p = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+
+file = open('latex_table.txt', 'w+')
+
+for pruning in p:
+    for merging in r:
+        with open(f'summary/Combination/Pruning{pruning:.1f}/ViT-Combine-Pruning{pruning:.1f}-Merging{merging}/metrics.txt') as f:
+            metrics = [0]*5
+            for i in range(4):
+                line = f.readline()
+                line = line.split(':')
+                line = line[1]
+                line = line.split(' ')
+                value1 = float(line[1])
+                value2 = float(line[3])
+                if i == 3:
+                    metrics[i] = f'{value1:.4f} \pm {value2:.3f}'
+                else:
+                    metrics[i] = f'{value1:.2f} \pm {value2:.3f}'
+                
+            line = f.readline()
+            line = line.split(':')
+            line = line[1]
+            metrics[4] = line[1:]
+            
+            file.write(f'ViT-B/16 (Prunned {int(100*pruning)}\%, r = {merging}) & ${metrics[4][:-1]}\%$ & {metrics[3].split(" ")[0]} & ${metrics[1]}$ & ${metrics[2]}$ & ${metrics[0]}$\\\\\hline\n')
+        
+    file.write('\n')
